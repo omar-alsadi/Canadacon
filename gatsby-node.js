dac23dd0201f6bd0c5a31950b4f60527ed037829
Omar AlSadi <66041816+omar-alsadi@users.noreply.github.com>
2021-02-04 20:54:46 -0500

    fixing the scrollreavel fn when build app

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,35 @@
 const path = require('path');
 
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
+    if (stage === 'build-html') {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /scrollreveal/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        });
+    }
+
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                '@components': path.resolve(__dirname, 'src/components'),
+                '@config': path.resolve(__dirname, 'src/config'),
+                '@images': path.resolve(__dirname, 'src/images'),
+                '@pages': path.resolve(__dirname, 'src/pages'),
+                '@styles': path.resolve(__dirname, 'src/components/styles'),
+            },
+        },
+    });
+};
+
+
 exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions;
 
